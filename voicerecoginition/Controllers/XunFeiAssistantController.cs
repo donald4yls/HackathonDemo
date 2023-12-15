14f2be23d7d2d120f96d07f321f8f3c521c9eb6e
Donald Yu <longsheng.yu@lego.com>
2023-12-15 17:27:10 +0800

    Add Voice recoginition by Feifei Tang.

---
+++ voicerecoginition/Controllers/XunFeiAssistantController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using XunFei;
+namespace voiceRecognition.Api;
+
+public static class VoiceRecognitionApi
+{
+    public static IEndpointRouteBuilder MapVoiceRecognitionApi(this IEndpointRouteBuilder builder)
+    {
+        var group = builder
+            .MapGroup("/VoiceRecognition")
+            .WithDescription("Voice Recognition API doc");
+
+        group.MapPost("/{voicefile}", async (string voicefile) =>
+        {
+            XunFeiAssistant xunFeiAssistant = new XunFeiAssistant() { testFile = voicefile };
+            var result = await xunFeiAssistant.GetResultAsync();
+            return TypedResults.Ok(result);
+        });
+
+        return group;
+    }
+}
+
